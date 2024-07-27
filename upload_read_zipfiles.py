e1b379ed1eb19e42b22a99425a8d5170f9a77b4d
Eric Winiecke <ewiniecke@gmail.com>
2024-07-27 18:03:00 -0500

    removed optional token line

--- upload_read_zipfiles.py
+++ upload_read_zipfiles.py
@@ -51,7 +51,6 @@ connection_string = (
 # S3 Configuration
 aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
 aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-aws_session_token = os.getenv("AWS_SESSION_TOKEN")  # Optional
 aws_region = os.getenv("AWS_REGION")
 bucket_name = os.getenv("S3_BUCKET_NAME")
 table_name_prefix = os.getenv("TABLE_NAME_PREFIX", "table_")
