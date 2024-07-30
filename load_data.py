193eee268f489c882699f9457623bda9f3093d78
Eric Winiecke <ewiniecke@gmail.com>
2024-07-30 18:50:27 -0500

    committing broken code to save

--- load_data.py
+++ load_data.py
@@ -7,17 +7,30 @@ Eric Winiecke
 """
 
 import os
-from time import perf_counter
 
 import pandas as pd
 from dotenv import load_dotenv
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
+
+
+def get_env_vars():
+    """
+    Assemble credentials for login
+    """
+    load_dotenv()
+    env_vars = {
+        "DATABASE_TYPE": os.getenv("DATABASE_TYPE"),
+        "DBAPI": os.getenv("DBAPI"),
+        "ENDPOINT": os.getenv("ENDPOINT"),
+        "USER": os.getenv("USER"),
+        "PASSWORD": os.getenv("PASSWORD"),
+        "PORT": int(os.getenv("PORT", 5432)),
+        "DATABASE": os.getenv("DATABASE"),
+    }
+    return env_vars
 
 
 def get_db_engine(env_vars):
-    """
-    get info to create connection string to database.
-    """
     connection_string = (
         f"{env_vars['DATABASE_TYPE']}+{env_vars['DBAPI']}://"
         f"{env_vars['USER']}:{env_vars['PASSWORD']}@"
@@ -27,50 +40,28 @@ def get_db_engine(env_vars):
     return create_engine(connection_string)
 
 
-def load_data_from_db(table_name, env_vars):
-    """
-    read in data
-    """
-    engine = get_db_engine(env_vars)
-    query = text(f"SELECT * FROM {table_name}")
-    return pd.read_sql(query, engine)
-
-
 def load_data(env_vars):
-    """
-    loading data into a dictionary
-    """
-    names = ["game_skater_stats", "game_plays", "game_shifts", "game"]
-    t2 = perf_counter()
-    df = {}
+    engine = get_db_engine(env_vars)
 
-    print("Loading data from PostgreSQL database...")
-    for name in names:
-        df[name] = load_data_from_db(name, env_vars).drop_duplicates(ignore_index=True)
-        t1, t2 = t2, perf_counter()
-        print(f"{name:>25}: {t2 - t1:.4g} sec, {len(df[name])} rows")
+    queries = {
+        "game_skater_stats": "SELECT * FROM game_skater_stats",
+        "game_plays": "SELECT * FROM game_plays",
+        "game_shifts": "SELECT * FROM game_shifts",
+        "game": "SELECT * FROM game",
+    }
+
+    df = {}
+    for name, query in queries.items():
+        df[name] = pd.read_sql(query, engine)
+        print(f"{name}:")
+        print(df[name].head())  # Print first few rows of each DataFrame for debugging
 
     return df
-
-
-def get_env_vars():
-    """
-    log into database
-    """
-    load_dotenv()
-    env_vars = {
-        "DATABASE_TYPE": os.getenv("DATABASE_TYPE"),
-        "DBAPI": os.getenv("DBAPI"),
-        "ENDPOINT": os.getenv("ENDPOINT"),
-        "USER": os.getenv("USER"),
-        "PASSWORD": os.getenv("PASSWORD"),
-        "PORT": os.getenv("PORT"),
-        "DATABASE": os.getenv("DATABASE"),
-    }
-    return env_vars
 
 
 if __name__ == "__main__":
     env_vars = get_env_vars()
     df = load_data(env_vars)
     print("Data loaded successfully.")
+    for name, df in df.items():
+        print(f"{name}: {len(df)} rows")
