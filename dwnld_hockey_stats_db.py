92f2c6cec8402a741c5df165cb6ba0625593f5be
Eric Winiecke <ewiniecke@gmail.com>
2024-10-06 09:36:07 -0500

    cleaning repo and organizing jupyter notebooks

--- dwnld_hockey_stats_db.py
+++ dwnld_hockey_stats_db.py
@@ -26,11 +26,12 @@ s3 = boto3.client(
 # Define bucket and file details
 bucket_name = os.getenv("S3_BUCKET_NAME")
 file_name = "hockey_stats_db.sql"
-local_file_name = "your_local_file_name"
+#local_file_name = "your_local_file_name"
+
+# Prompt the user to input the local file name
+local_file_name = input("Enter the local destination file name: ")
 
 s3_file_key = os.getenv("S3_FILE_KEY", "hockey_stats_db.sql")
-
-# Download the file
 s3.download_file(bucket_name, file_name, local_file_name)
 
 print(f"{file_name} has been downloaded as {local_file_name}")
