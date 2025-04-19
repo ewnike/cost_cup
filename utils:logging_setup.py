ea03e099f4fba0bf4993e49f24705ad57e5196ec
Eric Winiecke <ewiniecke@gmail.com>
2025-04-18 21:32:52 -0500

    removed unused file utils:logging_setup

--- utils:logging_setup.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-
-def setup_logging(filename="data_processing.log"):
-    logging.basicConfig(
-        filename=filename,
-        level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(message)s",
-    )
