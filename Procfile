25d28e78c2c2c016a6c2b4e376e03b39e6cdcab3
Eric Winiecke <ewiniecke@gmail.com>
2026-03-02 16:51:21 -0600

    deployment changes

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn application:application --bind 127.0.0.1:8000 --workers 1 --threads 4 --timeout 120
+web: gunicorn application:application --bind 127.0.0.1:8000 --worker-class gthread --workers 1 --threads 4 --timeout 120 --log-level debug --access-logfile - --error-logfile -
--- application.py
+++ application.py
@@ -1,3 +1,11 @@
-from dash_app.app import server
+# application.py  (at repo root)
+import os
+import sys
 
-application = server
+print("[BOOT] application.py starting")
+print("[BOOT] cwd:", os.getcwd())
+print("[BOOT] sys.path[0:5]:", sys.path[:5])
+
+from dash_app.app import server as application  # must succeed
+
+print("[BOOT] imported dash_app.app OK; application =", application)
